93f833eec793d59b5b42753b488b4f6aaae7eeb1
ShaliniManiChandanaKumari <147488173+ShaliniManiChandanaKumari@users.noreply.github.com>
2023-10-30 20:38:22 +0530

    #include<stdio.h> int main() {     int n,x;     scanf("%d%d",&n,&x);     if(n%2==0&&x%2==0)     printf("YES");     else if(n%2==0&&x%2!=0)     printf("YES");     else if(n%2!=0&&x%2!=0)     printf("YES");     else     printf("NO"); }

---
+++ Cut_the_Rope.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,x;
+    scanf("%d%d",&n,&x);
+    if(n%2==0&&x%2==0)
+    printf("YES");
+    else if(n%2==0&&x%2!=0)
+    printf("YES");
+    else if(n%2!=0&&x%2!=0)
+    printf("YES");
+    else
+    printf("NO");
+}
