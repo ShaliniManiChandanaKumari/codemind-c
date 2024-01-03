467f6e37f7fcb2b175ff754cb92fc25a3d12c135
ShaliniManiChandanaKumari <147488173+ShaliniManiChandanaKumari@users.noreply.github.com>
2024-01-03 10:07:04 +0530

    #include<stdio.h> int main() { 	int n,i,j; 	scanf("%d",&n); 	for(i=1;i<=n;i++) 	{ 		for(j=1;j<=n;j++) 		{ 			if(i==j || i+j==n+1) 			{ 				printf("%d ",i); 			} 			else 			{ 				printf(" "); 			} 		} 		printf(" "); 	} }

--- Pattern_8.1.c
+++ Pattern_8.1.c
@@ -1,54 +1,20 @@
 #include<stdio.h>
 int main()
 {
-	int a,b,i,j,n,c,d;
+	int n,i,j;
 	scanf("%d",&n);
-	c=n-1;
-	for(i=1;i<=(n/2)+1;i++)
+	for(i=1;i<=n;i++)
 	{
-		for(j=1;j<i;j++)
+		for(j=1;j<=n;j++)
 		{
-			printf(" ");
-		}
-		printf("%d ",i);
-		for(j=c;j>1;j--)
-		{
-			printf(" ");
-		}
-		c=c-2;
-		j=i;
-		if(j<=(n/2))
-		{
-			printf("%d ",i);
-		}
-		for(j=1;j<i;j++)
-		{
-			printf(" ");
-		}
-		printf("
-");
-	}
-	c=2;
-		for(i=(n/2)+2;i<=n;i++)
-	{
-		for(j=i;j<n;j++)
-		{
-			printf(" ");
-		}
-		printf("%d ",i);
-		for(j=1;j<c;j++)
-		{
-			printf(" ");
-		}
-		c=c+2;
-		j=i;
-		if(j>=(n/2)+2)
-		{
-			printf("%d ",i);
-		}
-		for(j=i;j<n;j++)
-		{
-			printf(" ");
+			if(i==j || i+j==n+1)
+			{
+				printf("%d ",i);
+			}
+			else
+			{
+				printf(" ");
+			}
 		}
 		printf("
 ");
